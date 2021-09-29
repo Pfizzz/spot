0784a5fbaf918a3cc479a60e4b5e28fc00959932
Justin Perez <perezjrp936@gmail.com>
2021-09-28 21:24:25 -0400

    corrected controllers and uncommented sequelize in server.js

--- server.js
+++ server.js
@@ -7,17 +7,17 @@ const exphbs = require('express-handlebars');
 const hbs = exphbs.create({ helpers });
 const session = require('express-session');
 
-// const SequelizeStore = require('connect-session-sequelize')(session.Store);
+ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-// const sess = {
-//   secret: 'Super secret secret',
-//   cookie: {},
-//   resave: false,
-//   saveUninitialized: true,
-//   store: new SequelizeStore({
-//     db: sequelize
-//   })
-// };
+ const sess = {
+   secret: 'Super secret secret',
+   cookie: {},
+   resave: false,
+   saveUninitialized: true,
+   store: new SequelizeStore({
+     db: sequelize
+   })
+ };
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -29,10 +29,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+
 // this must be run before the route.
-// app.use(session(sess));
+ app.use(session(sess));
 // // turn on routes-- this is usually the last thing before the you start the app.
-// app.use(routes);
+ app.use(routes);
 
 
 // turn on connection to db and server
