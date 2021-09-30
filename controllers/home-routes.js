fc21be1eb4903a4da9c5808868de8e6d9a1ad564
Justin Perez <perezjrp936@gmail.com>
2021-09-30 18:50:21 -0400

    made front page signup sheet work

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -9,14 +9,22 @@ router.get('/', (req, res) => {
 });
 
 router.get('/dogs', (req, res) => {
-  res.render('dogs');
+  res.render('dogs', {
+    loggedIn: req.session.loggedIn
+  });
 });
 
 router.get('/cats', (req, res) => {
-  res.render('cats');
+  res.render('cats', {
+    loggedIn: req.session.loggedIn
+  });
 });
 
 router.get('/login', (req, res) => {
+  if(req.session.loggedIn){
+    res.redirect('/');
+    return;
+  }
   res.render('login');
 });
 
